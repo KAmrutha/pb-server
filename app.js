f41ac9d4a8372aba6f899cdaf1159eb8ce7a736c
KAmrutha <157336098+KAmrutha@users.noreply.github.com>
2024-04-30 14:06:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 //cors setting
 app.use(cors());
 //db
-mongoose.connect("mongodb+srv://User1:reddy@personalbudget.un7ntvf.mongodb.net/?retryWrites=true&w=majority&appName=PERSONALBUDGET")
+mongoose.connect("mongodb+srv://reddy1234:Reddy1234@personalbudget.un7ntvf.mongodb.net/?retryWrites=true&w=majority&appName=PERSONALBUDGET")
   .then(() => {
     console.log("Connected to the database");
   })
